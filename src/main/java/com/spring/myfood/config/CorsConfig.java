19b367b704c5f0b1dc48065a666fa546e9201046
Jessie <jessie.moura19@gmail.com>
2024-03-11 11:15:29 -0400

    fix: new attempt to deploy project

--- src/main/java/com/spring/myfood/config/CorsConfig.java
+++ src/main/java/com/spring/myfood/config/CorsConfig.java
@@ -14,7 +14,6 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
